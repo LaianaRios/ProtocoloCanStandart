bea9b7c603c5fa3a1f2a74d748fd70bbff2f41fc
LaianaRios <57690299+LaianaRios@users.noreply.github.com>
2019-11-13 13:27:07 -0300

    Update CAN 2.0A - RECEPTOR.py

--- CAN 2.0A - RECEPTOR.py
+++ CAN 2.0A - RECEPTOR.py
@@ -5,27 +5,47 @@
 # --------- Equipe: Laiana Rios, Robson Barbosa, Samuel Dias -------
 # --------------------- Código do Receptor -------------------------
 
+# Importar o Socket para poder se comunicar com o servidor
 import socket
 
+# Do arquivo fGeral irá importar todas as funções e anexar ao arquivo 
 from fGeral import *
 
-HOST = "127.0.0.1"                         # Endereco IP do Servidor
-PORT = 5000                                 # Porta que o Servidor está
+# Endereco IP do Servidor
+HOST = "127.0.0.1"  
 
+# Porta que o Servidor estar
+PORT = 5000                                 
+
+# Irá se conectar ao servidor e permanecerá conectado
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Faz conexão ao servidor
 orig = (HOST, PORT)
+
+# Informa que está aberto para receber dados 
 tcp.bind(orig)
 
 print("SERVER INICIADO")
+
+# Fica a espera dos dados
+# 1 = Só irá aceitar 1 cliente 
 tcp.listen(1)
 
+# Irá executar o código enquanto o servidor estiver aberto
 while True:
+    
+    # Variáveis definidas quando se estabelece uma conexão
     con, cliente = tcp.accept()
     print ('CONECTADO POR', cliente)
 
+    # Recebimento dos dados
     while True:
+        
+        # 1024 = Faixa de comunicação
         msg = con.recv(1024)
+        
+        # Se a mensagem não existir, o código dará um break
         if not msg: break
 
         print("MENSAGEM RECEBIDA: ", cliente, msg)
