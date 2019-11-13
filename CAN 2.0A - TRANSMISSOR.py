c2e53fe2592bbacf299e0e3ba4d8d9587d52ef00
LaianaRios <57690299+LaianaRios@users.noreply.github.com>
2019-11-13 13:16:04 -0300

    Update CAN 2.0A - TRANSMISSOR.py

--- CAN 2.0A - TRANSMISSOR.py
+++ CAN 2.0A - TRANSMISSOR.py
@@ -5,15 +5,24 @@
 # --------- Equipe: Laiana Rios, Robson Barbosa, Samuel Dias -------
 # --------------------- Código do Transmissor ----------------------
 
+#-------Esse código representa o cliente (Envio dos dados)----------
+
+# Importar o Socket para poder se comunicar com o servidor e obter uma resposta de volta
 import socket
 
+# Do arquivo fGeral irá importar todas as funções e anexar ao arquivo 
 from fGeral import *
 
-HOST = "127.0.0.1"                         # Endereco IP do Servidor
-PORT = 5000                                # Porta que o Servidor está
+# Endereco IP do Servidor
+HOST = "127.0.0.1"    
 
+ # Porta que o Servidor estar
+PORT = 5000   
+
+# Irá se conectar ao servidor e permanecerá conectado
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Faz conexão ao servidor
 dest = (HOST, PORT)
 tcp.connect(dest)
 
@@ -24,7 +33,8 @@ flag = True
 while flag:
 
     print(("-")*150)
-    # Envia uma mensagem para o servidor
+    
+    # Recebe a mensagem do cliente e armazena na variavel msg
     msg = input("DIGITE A MENSAGEM EM BINARIO QUE VOCÊ DESEJA ENVIAR: ")
 
     if(msg == "\x18"):
@@ -33,17 +43,18 @@ while flag:
 
     print("MENSAGEM RECEBIDA:    ",msg)
 
-    #ENTRA COM O VERIFICADOR DE SEGURANCA
+    # Entra com o verificador de segurança
     paridades(msg)
 
-    #DEFINE A CHAVE
+    # Define o CRC
     key = "1100010110011001"
 
-    #CODIFICA O CODIGO
+    # encodeData = Codificação da mensagem
     ans = encodeData(msg, key)
 
     print("MENSAGEM COM CRC:    ",ans)
     
+    # Faz o encapsulamento da mensagem
     encap = "0100110001000001000"
     encap2 = "011111111"
     
@@ -51,8 +62,13 @@ while flag:
     
     print("MENSAGEM ENCAPSULADA E ENVIADA:    ",ans)
 
+    # Faz o envio da variável ans através do servidor
+    # encode = Faz a conversão para binário
     tcp.send(ans.encode('UTF-8'))
     
+    # Resposta do servidor
+    # 1024 = Faixa de comunicação do servidor 
     print (str(tcp.recv(1024),"UTF-8"))
 
+# Encerra o servidor
 tcp.close()
